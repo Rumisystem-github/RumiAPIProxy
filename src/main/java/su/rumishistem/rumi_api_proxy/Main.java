4d32c28b826e96e20ddf4ea525d085d651f97cf1
SINtyanneru <irjitgd@gmail.com>
2025-12-15 14:30:14 +0900

    Init

---
+++ src/main/java/su/rumishistem/rumi_api_proxy/Main.java
@@ -0,0 +1,7 @@
+package su.rumishistem.rumi_api_proxy;
+
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ start.sh
@@ -0,0 +1,1 @@
+java -jar target/rumi_api_proxy-1.0-jar-with-dependencies.jar
